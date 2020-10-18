cbbf4049b009d412102cce913865d129e5e9e40c
Diego Giuliani <giuliani.diego@gmail.com>
2020-10-18 09:15:20 -0300

    Add Icon context in navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -12,36 +12,39 @@ import {
 } from './syled';
 import { FaBars } from 'react-icons/fa';
 import { useScrollNav } from '../../hooks/useScrollNav';
+import { IconContext } from 'react-icons/lib';
 interface Props {
   onMenuClick: () => void;
 }
 export default function Navbar({ onMenuClick }: Props): ReactElement {
   const scrollNav = useScrollNav(180);
   return (
-    <Nav scrollNav={scrollNav}>
-      <NavbarContainer>
-        <NavbarLogo to="/">dolla</NavbarLogo>
-        <MenuButton onClick={() => onMenuClick()}>
-          <FaBars />
-        </MenuButton>
-        <NavMenu>
-          <NavItem>
-            <NavLink to="about">About</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="discover">Discover</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="services">Services</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="signup">Sign up</NavLink>
-          </NavItem>
-        </NavMenu>
-        <NavBtn>
-          <NavBtnLink to="/signin">Sign in</NavBtnLink>
-        </NavBtn>
-      </NavbarContainer>
-    </Nav>
+    <IconContext.Provider value={{ color: '#fff' }}>
+      <Nav scrollNav={scrollNav}>
+        <NavbarContainer>
+          <NavbarLogo to="/">dolla</NavbarLogo>
+          <MenuButton onClick={() => onMenuClick()}>
+            <FaBars />
+          </MenuButton>
+          <NavMenu>
+            <NavItem>
+              <NavLink to="about">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="discover">Discover</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="services">Services</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="signup">Sign up</NavLink>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign in</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </IconContext.Provider>
   );
 }
